6e57d834e7d46f6ea02cfc5ae5d3e2d308365277
Nesenur-arg <nesenurargis69@gmail.com>
2024-12-31 14:41:08 +0300

    Add files via upload

---
+++ sketch_dec31a/sketch_dec31a.ino
@@ -0,0 +1,33 @@
+int sari=3;
+int kirmizi = 4;
+String girilen ="";
+
+void setup()
+{
+ Serial.begin(9600);
+
+pinMode(sari,OUTPUT);
+pinMode(kirmizi,OUTPUT);
+Serial.print("sari icin s kirmizi icin k ye bas = ");
+}
+
+void loop() 
+{
+if(Serial.available()){
+  girilen=Serial.readString();
+  Serial.print("sari icin s kirmizi icin k ye bas = ");
+  Serial.print(girilen);
+ if(girilen=="s"){
+  Serial.print("SARI YANDI");
+  digitalWrite(sari,1);
+  delay(3000);
+  digitalWrite(sari,0);
+ }
+ if(girilen=="k"){
+  Serial.println("KIRMIZI YANDI");
+    digitalWrite(kirmizi,1);
+    delay(3000);
+  digitalWrite(kirmizi,0);
+  }
+}
+}
